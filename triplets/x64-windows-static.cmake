6e55c7ea5b440b098b871422282f9e48db9c826c
martona <marton@anka.me>
2026-05-21 23:34:08 -0400

    further decrease binary size

--- triplets/x64-windows-static.cmake
+++ triplets/x64-windows-static.cmake
@@ -4,6 +4,13 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 if(PORT STREQUAL "qtbase")
     list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS
+        "-DFEATURE_opengl:BOOL=OFF"
         "-DFEATURE_timezone:BOOL=OFF"
+    )
+endif()
+
+if(PORT STREQUAL "qtdeclarative")
+    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS
+        "-DFEATURE_qml_debug:BOOL=OFF"
     )
 endif()
