4429ac69ae1f8020628008ec0fba0b88b7767396
martona <marton@anka.me>
2026-05-21 20:01:29 -0400

    size reduction

---
+++ triplets/x64-windows-static.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+if(PORT STREQUAL "qtbase")
+    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS
+        "-DFEATURE_timezone:BOOL=OFF"
+    )
+endif()
